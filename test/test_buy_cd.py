73ecb79d471946cc0589135211da63d79aae9b40
patbaker <patrick.baker@macquarie.com>
2022-03-04 13:09:36 +0000

    payments accepted

--- test/test_buy_cd.py
+++ test/test_buy_cd.py
@@ -1,12 +1,28 @@
 import unittest
 
 class CD:
-    def buy(self, quantity):
+    def buy(self, quantity, payment):
         return True
+
+class PaymentService:
+    pass
+
+class Payment:
+    def __init__(self, value, payment_service):
+        self.value = value
+        self.payment_service = payment_service
 
 
 class TestBuyCD(unittest.TestCase):
     def test_buy_cd(self):
         cd = CD()
+        payment_service = PaymentService()
+        payment = Payment(value=10, payment_service=payment_service)
+        self.assertTrue(cd.buy(quantity=3, payment=payment), True)
 
-        self.assertTrue(cd.buy(quantity=3), True)
+    def test_buy_cd_if_payment_is_accepted(self):
+        cd = CD()
+        payment_service = PaymentService()
+        payment = Payment(value=10, payment_service=payment_service)
+
+        self.assertTrue(cd.buy(quantity=1, payment=payment), True)
